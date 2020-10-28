23e8f4a8a043b377f4b3378acd1ff292707b375e
Payal Ghusalikar <payal.ghusalikar9@gmail.com>
2020-10-28 17:33:33 +0530

    Added : interface for AddressBookManager

--- addressbook/AddressBookManagerInterface.java
+++ addressbook/AddressBookManagerInterface.java
@@ -1,5 +1,11 @@
 package addressbook;
 
 public interface AddressBookManagerInterface {
-
+	public void displayAddressBook();
+	public void newAddressBook();
+	public void openAddressBook();
+	public void saveAddressBook();
+	public void saveasAddressBook();
+	public void closeAddressBook();
+	public void quit();
 }
