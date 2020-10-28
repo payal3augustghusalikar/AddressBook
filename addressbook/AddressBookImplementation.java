dc749ee09509bcc1d3de921715bf8d1e4e4cc848
Payal Ghusalikar <payal.ghusalikar9@gmail.com>
2020-10-28 17:50:17 +0530

    Added : Implementation for addressBookManager

--- addressbook/AddressBookImplementation.java
+++ addressbook/AddressBookImplementation.java
@@ -1,5 +1,65 @@
 package addressbook;
+import java.util.*;
 
-public class AddressBookImplementation {
+class AddressBookImplementation implements AddressBookInterface {
+	Person p;
+	private Object name;
+	private Scanner inp;
+	public static ArrayList<Person> addressBook = new ArrayList<Person>();
+	// addressBook = new ArrayList<Person>();
+    
+	public AddressBookImplementation() {
+		 ArrayList<Person> addressBook = new ArrayList<Person>();	 
+	}
+	public void addPerson() {
+		 Scanner scanner = new Scanner(System.in);
+		 System.out.println("enter your first name:");
+	     String firstName = scanner.nextLine();
+	     System.out.println("enter your last name:");
+	     String lastName = scanner.nextLine();
+	     System.out.println("enter your city");
+	     String city = scanner.nextLine();
+	     System.out.println("enter your state");
+	     String state = scanner.nextLine();
+	     System.out.println("enter your phone");
+	     long MobileNo = scanner.nextLong();
+	     System.out.println("enter your zip code");
+	     int zip = scanner.nextInt();
 
+	     Person person = new Person(firstName, lastName, city, state, MobileNo, zip);
+	     addressBook.add(person);
+	   //  System.out.println(addressBook);
+	}
+public void deletePerson() {
+		
+    }	
+	
+	public void sortByName() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void sortByZip() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void searchPerson() {
+
+	}
+
+	public void display() {
+		System.out.println("Entered Person Details is:");
+			for(Person p : addressBook) {
+				System.out.println(p.toString());
+				
+			//	System.out.println(addressBook.get(i));	
+			}
+	
+	}
+	public void quit() {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
--- addressbook/AddressBookManagerImp.java
+++ addressbook/AddressBookManagerImp.java
@@ -1,5 +1,110 @@
 package addressbook;
 
-public class AddressBookManagerImp {
+import java.util.Scanner;
 
-}
+public class AddressBookManager {
+	
+	public void displayAddressBook() {
+	}
+	public void newAddressBook() {
+		int loop=0;
+		while (loop == 0) {
+			AddressBookImplementation Menu = new AddressBookImplementation();
+			System.out.println("Address Book"
+							+ "\n1. add Person"
+							+ "\n2. edit Person"
+							+ "\n3. delete Person"
+							+ "\n4. sort ByName"
+							+ "\n5. sort ByZip"
+							+ "\n6. search Person"
+							+ "\n7. Quit"
+							+ "\n8. display"
+							+ "\n. select any choice ");
+			
+			Scanner input = new Scanner(System.in); 
+
+			int num = input.nextInt();
+			switch (num) {
+			case 1:
+				Menu.addPerson();
+				break;
+			case 2:
+				Menu.editPerson();
+				break;
+			case 3:
+				Menu.deletePerson();
+				break;
+			case 4:
+				Menu.sortByName();
+				break;
+			case 5:
+				Menu.sortByZip();
+				break;
+			case 6:
+				Menu.searchPerson();
+				break;
+			case 7:
+				Menu.quit();
+				break;
+			case 8:
+				Menu.display();
+				
+				break;
+			default:
+				System.out.println("please enter right choice");
+			}
+		
+	}
+	}
+	
+	
+	private void display() {
+		// TODO Auto-generated method stub
+		
+	}
+	private void searchPerson() {
+		// TODO Auto-generated method stub
+		
+	}
+	private void sortByZip() {
+		// TODO Auto-generated method stub
+		
+	}
+	private void sortByName() {
+		// TODO Auto-generated method stub
+		
+	}
+	private void deletePerson() {
+		// TODO Auto-generated method stub
+		
+	}
+	private void editPerson() {
+		// TODO Auto-generated method stub
+		
+	}
+	private void addPerson() {
+		// TODO Auto-generated method stub
+		
+	}
+	public void saveAddressBook() {
+		
+	}
+	public void saveasAddressBook() {
+		
+	}
+	public void closeAddressBook() {
+		
+	}
+	public void quit() {
+		
+	}
+	public void openAddressBook() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+		
+	
+	}
+
+	
